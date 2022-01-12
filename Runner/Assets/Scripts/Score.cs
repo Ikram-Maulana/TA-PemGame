e7f178a2e6d5d00fa829f913ea6583b6b8a57b20
ikram-maulana <ikram075@ummi.ac.id>
2022-01-12 12:21:49 +0700

    Add HighScore and fonts

---
+++ Runner/Assets/Scripts/Score.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+  public Transform player;
+  public Text scoreText;
+  public Text highScoreText;
+
+  float score = 0;
+  float highScore = 0;
+
+  void Start()
+  {
+    highScore = PlayerPrefs.GetFloat("highScore", 0);
+    score = Mathf.Round(player.position.z); 
+    scoreText.text = score.ToString("0") + " Points";
+    highScoreText.text = "Highscore: " + highScore.ToString("0");
+  }
+  
+  void Update()
+  {
+    score = Mathf.Round(player.position.z);
+    scoreText.text = score.ToString("0") + " Points";
+    if(highScore < score) {
+      PlayerPrefs.SetFloat("highScore", score);
+    }
+  }
+}
+
