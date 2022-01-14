53c9c707ee8498c6f17987c2768142fe58f1d998
ikram-maulana <ikram075@ummi.ac.id>
2022-01-14 18:22:30 +0700

    Fix Menu Improve

--- Runner/Assets/Scripts/mainMenuScript.cs
+++ Runner/Assets/Scripts/mainMenuScript.cs
@@ -2,15 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class mainMenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
-public void MulaiGame(){
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-}
-public void KeluarGame(){
-    Application.Quit();
-    Debug.Log("keluar");
+    public void MulaiGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-}
+    public void RestartGame()
+    {
+      SceneManager.LoadScene("SampleScene");
+    }
+
+    public void AboutGame()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void BackMenuBtn()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void BackMenuBtnFromGame()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void KeluarGame()
+    {
+        Application.Quit();
+        Debug.Log("keluar");
+    }
 }
